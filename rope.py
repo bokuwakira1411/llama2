35b7b1d634f93701d607d5cfc73a66a72c90194c
Nguyễn Thị Ngọc Lan <158143866+bokuwakira1411@users.noreply.github.com>
2026-04-13 21:47:54 +0700

    Update rope.py

--- rope.py
+++ rope.py
@@ -1,6 +1,28 @@
 from typing import Tuple
 import torch
 
+
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
+    """
+    Helper function to reshape frequency tensor to have the same shape as the target tensor 'x'
+    for the purpose of broadcasting the frequency tensor during element-wise operations.
+
+    Args:
+        freqs_cis (torch.Tensor): Frequency tensor to be reshaped.
+        x (torch.Tensor): Target tensor for broadcasting compatibility.
+
+    Returns:
+        torch.Tensor: Reshaped frequency tensor.
+
+    Raises:
+        AssertionError: If the frequency tensor doesn't match the expected shape.
+        AssertionError: If the target tensor 'x' doesn't have the expected number of dimensions.
+    """
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(shape)
 def apply_rotary_emb(
     query: torch.Tensor,
     key: torch.Tensor,
